4a718a65327fe934116637d36538a90ff3c1c336
Lloyd Francis <lloydaf@gmail.com>
2018-11-07 10:12:40 +0530

    Adding pipeall function

--- src/functions/pipe.ts
+++ src/functions/pipe.ts
@@ -3,9 +3,26 @@ declare module "pipe" {
   export = Pipe;
 }
 interface Object {
-  pipe(fn: Function): Object;
+  pipe(fn: Function, thisArg?: Object): Object;
+  pipeAll(...arguments: Array<Function>): Object;
 }
-
+/**
+ * @param fn - Takes in a function to pipe the object to
+ * @returns - Returns the resultant object of the operation after piping through the function.
+ */
 Object.prototype.pipe = function(fn: Function): Object {
-  return fn(this);
+  let obj = this;
+  return fn(obj);
+};
+
+/**
+ * @param arguments - Takes in multiple comma separated functions to pipe one after the other
+ * @returns - Returns the resultant object after piping through all the functions.
+ */
+Object.prototype.pipeAll = function(): Object {
+  let obj = this;
+  for (let i = 0; i < arguments.length; ++i) {
+    obj = arguments[i](obj);
+  }
+  return obj;
 };
--- tests/functions/pipe.test.ts
+++ tests/functions/pipe.test.ts
@@ -2,21 +2,24 @@ import "../../src/functions/pipe";
 function addOne(num: number) {
   return num + 1;
 }
+function addTwo(num: number) {
+  return num + 2;
+}
+function square(num: number) {
+  return num * num;
+}
 test("Simple Composition", () => {
-  function addTwo(num: number) {
-    return num + 2;
-  }
-
   expect(addOne(2).pipe(addTwo)).toEqual(5);
 });
 
 test("Multiple Composition", () => {
-  function square(num: number) {
-    return num * num;
-  }
   expect(
     addOne(3)
       .pipe(square)
       .pipe(square)
   ).toEqual(256);
+});
+
+test("Pipe All with two functions", () => {
+    expect(addOne(1).pipeAll(addTwo,square)).toEqual(16);
 });
