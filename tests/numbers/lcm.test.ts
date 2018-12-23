5b89b2f5c081f8c65166603199eaae536686bdcf
Lloyd Francis <lloydaf@gmail.com>
2018-12-23 20:09:47 +0530

    adding a test case for lcm

--- tests/numbers/lcm.test.ts
+++ tests/numbers/lcm.test.ts
@@ -9,3 +9,9 @@ test("LCM of two digit numbers", () => {
   expect(lcm(10, 15)).toEqual(30);
   expect(lcm(10, 15, 20)).toEqual(60);
 });
+
+test("LCM of the first twenty numbers", () => {
+  const arr = [];
+  for(let i = 0;i<20;++i) arr.push(i+1);
+  expect(lcm(...arr)).toEqual(232792560);
+});
